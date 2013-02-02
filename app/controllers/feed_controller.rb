8c56d3f5658218455dea09823cb9d9ee76301677
freen <dbickett@gmail.com>
2013-02-02 13:27:27 -0500

    Removed rel=nofollow attribute addition from HTML sanitizer.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,24 +4,25 @@ class FeedController < ApplicationController
 
 	def entries_all
 		do_scrape
-	    @posts = UbuEntry.all(:order => "created_at DESC", :limit => 400) 
-	    render :layout => false, :content_type => "application/rss+xml"
+	    @posts = UbuEntry.all(:order => "created_at DESC", :limit => 400)
+	    render :layout => false, :content_type => Mime::RSS
 	end
 
 	private
 
 		def set_sanitizer
-			@sanitizer = Sanitize.new(Sanitize::Config::BASIC)
+			sanitize_config = Sanitize::Config::BASIC
+			# No need for nofollow
+			sanitize_config[:add_attributes]["a"].delete('rel')
+			@sanitizer = Sanitize.new(sanitize_config)
 		end
 
 		def scheduled_scrape_is_due
+			last_scrape = ScrapeEvent.last
+			return true if last_scrape.nil?
 
 			# How long since we last scraped?
 			now = Time.new
-			last_scrape = ScrapeEvent.last
-
-			return true if last_scrape.nil?
-
 			minutes_since_last_scrape = ((now - last_scrape.created_at) / 1.minute).round		
 			scrape_interval_minutes = Setting::getIntegerValue('scrape_interval_minutes')
 			return minutes_since_last_scrape >= scrape_interval_minutes
@@ -29,9 +30,7 @@ class FeedController < ApplicationController
 
 		def do_scrape
 			if scheduled_scrape_is_due
-
 				scrape_event = ScrapeEvent.new
-
 				# Scrape ubu.com!
 				parser = UbuParse.new
 				content_sets = parser.extract
