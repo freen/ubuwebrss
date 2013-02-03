a106363653005d3e821720ffaadbb7e77d54e1a8
freen <dbickett@gmail.com>
2013-02-03 01:13:25 -0500

    Fixed routes for new action name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 UbuWebRSS::Application.routes.draw do
   root :to => 'index#index'
   scope :format => true, :constraints => { :format => 'rss' } do
-    get 'feed/all' => 'feed#news'
+    get 'feed/news' => 'feed#news'
   end
 end
