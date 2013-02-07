1ddc437cb3fe5849f58303cfe3d073e2bfc484da
freen <dbickett@gmail.com>
2013-02-06 20:04:50 -0500

    Check to ensure there is an item description before manipulating it. Added Artist name to entry title line.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,13 +4,13 @@ class IndexController < ApplicationController
 
 	def index
 		do_scrape
-		@latest_entries = UbuEntry.limit(5).order('id desc')
+		@latest_entries = UbuEntry.limit(7).order('id desc')
 	end
 
 	private
 
 		def set_scrape_log
 			@log_last_scrape = ScrapeEvent.last
-			@log_recent_scrapes_with_entries = ScrapeEvent.where('ubu_entries_count > 0').limit(5).order('id desc')
+			@log_recent_scrapes_with_entries = ScrapeEvent.where('ubu_entries_count > 0').limit(1).order('id desc')
 		end
 end
